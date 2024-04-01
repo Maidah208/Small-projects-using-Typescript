f47deafbf451f9e5fb822c0ea86d68ed1e40adc4
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-04-01 22:50:42 +0500

    Add files via upload

---
+++ BMI Calculator/index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        message: "Enter your weight in Kg. ",
+        name: "weight",
+        type: "number"
+    },
+    {
+        message: "Enter Your Height in Meters:",
+        name: "height",
+        type: "number"
+    },
+]);
+let bmi = (answer.weight / (answer.height * answer.height)).toFixed(2);
+console.log(`Your BMI is ${bmi}.`);
